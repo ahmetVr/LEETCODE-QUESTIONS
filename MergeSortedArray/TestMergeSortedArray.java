cfdf44bb12c6ae87b2f9faf9195aca57a3bbe40b
ahmetvar <vienasdutrys10@gmail.com>
2024-11-03 22:43:33 +0300

    Merge Sorted Array

---
+++ MergeSortedArray/TestMergeSortedArray.java
@@ -0,0 +1,31 @@
+package MergeSortedArray;
+
+public class TestMergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0}; 
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+
+
+        int[] nums3 = {1};
+        int m1 = 1;
+        int[] nums4 = {};
+        int n1 = 0;
+
+        MergeSortedArray test = new MergeSortedArray();
+        test.merge(nums1, m, nums2, n);
+
+        test.merge(nums3, m1, nums4, n1);
+
+        for(int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + " ");
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < nums3.length; i++) {
+            System.out.print(nums3[i] + " ");
+        }
+    }
+}
